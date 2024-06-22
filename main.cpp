2ab43808398302d31d4a3d7d1d836aaa4dcb5ed6
Kostiantyn Rakilov <rakilovkg@gmail.com>
2024-06-22 21:48:15 +0300

    Added boilerplate code

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
-// g++ main.cpp -o main.exe -Iinclude -Llib -lvulkan-1 -lglfw3 -lgdi32 && main.exe
-
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main(int argc, char *argv[]) {
-  glfwInit();
-  
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window = glfwCreateWindow(512, 512, "Моя супер гра!", nullptr, nullptr);
-  
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-  std::cout << "extension count: " << extensionCount << std::endl;
-  
-  glm::mat4 matrix;
-  glm::vec4 vector;
-  auto test = matrix * vector;
-  
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+class Application {
+public:
+  void run() {
+	initializeVulkan();
+	runMainLoop();
+	cleanup();
+  }
+
+private:
+  void initializeVulkan() {
   }
   
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+  void runMainLoop() {
+	
+  }
+  
+  void cleanup() {
+	
+  }
+  
+  VkInstance vkInstance;
+};
+
+int main(int argc, char *argv[]) {
+  Application application;
+  try {
+	application.run();
+  } catch (const std::excepttion& exception) {
+	std::cerr << exception.what() << std::endl;
+	return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
 }
