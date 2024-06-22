3e2e02c44027365910556ac900e964a696d40c91
Kostiantyn Rakilov <rakilovkg@gmail.com>
2024-06-22 22:00:44 +0300

    Implemented main loop

--- main.cpp
+++ main.cpp
@@ -1,30 +1,46 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WINDOW_WIDTH = 512;
+const uint32_t WINDOW_HEIGHT = 512;
+
 class Application {
 public:
   void run() {
+	initializeWindow();
 	initializeVulkan();
 	runMainLoop();
 	cleanup();
   }
 
 private:
+  void initializeWindow() {
+    glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan Application", nullptr, nullptr);
+  }
+  
   void initializeVulkan() {
+	  
   }
   
   void runMainLoop() {
-	
+	while (!glfwWindowShouldClose(window)) {
+	  glfwPollEvents();
+	}
   }
   
   void cleanup() {
-	
+	glfwDestroyWindow(window);
+	glfwTerminate();
   }
   
-  VkInstance vkInstance;
+  GLFWwindow *window;
 };
 
 int main(int argc, char *argv[]) {
