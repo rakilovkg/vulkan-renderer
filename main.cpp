cbcfd0964538ce2913081dac090cd5e367db0ccd
Kostiantyn Rakilov <rakilovkg@gmail.com>
2024-06-23 12:04:52 +0300

    Implemented instance creation

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
 const uint32_t WINDOW_WIDTH = 512;
 const uint32_t WINDOW_HEIGHT = 512;
 
@@ -26,7 +28,55 @@ private:
   }
   
   void initializeVulkan() {
+	createInstance();
+  }
+  
+  void createInstance() {
+	VkApplicationInfo applicationInfo{};
+	applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	applicationInfo.pApplicationName = "Vulkan App";
+	applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	applicationInfo.pEngineName = "No Engine";
+	applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	applicationInfo.apiVersion = VK_VERSION_1_0;
 	  
+	VkInstanceCreateInfo instanceCreateInfo{};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pApplicationInfo = &applicationInfo;
+	
+	instanceCreateInfo.enabledLayerCount = 0;
+	
+	uint32_t glfwExtensionCount = 0;
+	const char **glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	
+	std::vector<const char*> requiredExtensions;
+	
+	for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+		requiredExtensions.emplace_back(glfwExtensions[i]);
+	}
+	
+	/*
+	-- MacOS --
+	requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+	instanceCreateInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+	*/
+	instanceCreateInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+	instanceCreateInfo.ppEnabledExtensionNames = requiredExtensions.data();
+	
+	if (vkCreateInstance(&instanceCreateInfo, nullptr, &instance) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create Vulkan instance.");
+	}
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	std::cout << "Available extensions:" << std::endl;
+	for (const auto& extension : extensions) {
+	  std::cout << extension.extensionName << std::endl;
+	}
   }
   
   void runMainLoop() {
@@ -36,18 +86,20 @@ private:
   }
   
   void cleanup() {
+	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
 	glfwTerminate();
   }
   
   GLFWwindow *window;
+  VkInstance instance;
 };
 
 int main(int argc, char *argv[]) {
   Application application;
   try {
 	application.run();
-  } catch (const std::excepttion& exception) {
+  } catch (const std::exception& exception) {
 	std::cerr << exception.what() << std::endl;
 	return EXIT_FAILURE;
   }
